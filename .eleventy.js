5b7c0847d8b420697610cfbb6110914726172361
Glitch (mattjibson-com) <none>
2024-04-15 17:52:13 +0000

    📳🕠 Updated with Glitch

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,13 @@
 const { DateTime } = require("luxon");
-const pluginSEO = require("eleventy-plugin-seo");
 
 /**
-* This is the JavaScript code that determines the config for your Eleventy site
-*
-* You can add lost of customization here to define how the site builds your content
-* Try extending it to suit your needs!
-*/
+ * This is the JavaScript code that determines the config for your Eleventy site
+ *
+ * You can add lost of customization here to define how the site builds your content
+ * Try extending it to suit your needs!
+ */
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.setTemplateFormats([
     // Templates:
     "html",
@@ -22,43 +21,42 @@ module.exports = function(eleventyConfig) {
     "png",
     "svg",
     "woff",
-    "woff2"
+    "woff2",
   ]);
   eleventyConfig.addPassthroughCopy("public");
-
-  /* From: https://github.com/artstorm/eleventy-plugin-seo
-  
-  Adds SEO settings to the top of all pages
-  The "glitch-default" bit allows someone to set the url in seo.json while
-  still letting it have a proper glitch.me address via PROJECT_DOMAIN
-  */
-  const seo = require("./src/seo.json");
-  if (seo.url === "glitch-default") {
-    seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-  }
-  eleventyConfig.addPlugin(pluginSEO, seo);
+  eleventyConfig.addPassthroughCopy(".well-known");
 
   // Filters let you modify the content https://www.11ty.dev/docs/filters/
-  eleventyConfig.addFilter("htmlDateString", dateObj => {
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
 
-  eleventyConfig.setBrowserSyncConfig({ ghostMode: false });
+  eleventyConfig.setBrowserSyncConfig({
+    ghostMode: false,
+
+    middleware: [
+      function (req, res, next) {
+        if (req.url.startsWith('/.well-known/')) {
+          res.setHeader("Content-Type", "application/json");
+        }
+        next();
+      },
+    ],
+  });
 
   /* Build the collection of posts to list in the site
      - Read the Next Steps post to learn how to extend this
   */
-  eleventyConfig.addCollection("posts", function(collection) {
-    
+  eleventyConfig.addCollection("posts", function (collection) {
     /* The posts collection includes all posts that list 'posts' in the front matter 'tags'
        - https://www.11ty.dev/docs/collections/
     */
-    
+
     const coll = collection
       .getFilteredByTag("posts")
       .sort((a, b) => b.data.date - a.data.date);
 
-    // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426 
+    // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426
     // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
     for (let i = 0; i < coll.length; i++) {
       const prevPost = coll[i - 1];
@@ -75,7 +73,7 @@ module.exports = function(eleventyConfig) {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "build"
-    }
+      output: "build",
+    },
   };
 };
